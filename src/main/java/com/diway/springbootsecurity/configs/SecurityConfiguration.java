3d1563f47ae93abe7ca702d03924e6b041e97110
Diway Sanu <diwaygrover0019@gmail.com>
2020-03-12 12:15:17 +1100

    Setting JDBC authentication using H2 database with default schema

--- src/main/java/com/diway/springbootsecurity/configs/SecurityConfiguration.java
+++ src/main/java/com/diway/springbootsecurity/configs/SecurityConfiguration.java
@@ -1,23 +1,37 @@
 package com.diway.springbootsecurity.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Set in-memory configuration on the auth object
-        auth.inMemoryAuthentication()
+        /*auth.inMemoryAuthentication()
                 .withUser("blah").password("blah").roles("USER")
                 .and()
-                .withUser("foo").password("foo").roles("ADMIN");
+                .withUser("foo").password("foo").roles("ADMIN");*/
+
+        // Using H2 database with default schema and creating bunch of users
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("user").password("pass").roles("USER"))
+                .withUser(User.withUsername("admin").password("pass").roles("ADMIN"));
     }
 
     @Bean
